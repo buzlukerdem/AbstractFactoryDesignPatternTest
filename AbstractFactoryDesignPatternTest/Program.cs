a6084d7badb4ec2bd0f36a7a7af3c4e241a22f78
buzlukerdem <buzlukerdem@gmail.com>
2025-01-23 17:16:43 +0300

    abstract factory with factory method(Creator class parameter get ComputerType enum.)

--- AbstractFactoryDesignPatternTest/Program.cs
+++ AbstractFactoryDesignPatternTest/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 ComputerCreator creator = new();
-Computer dragon60 = creator.CreateComputer(new DRAGON4060());
-Console.WriteLine(dragon60.VideoCard.VideoCardName);
+Computer drg4060 = creator.CreateComputer(ComputerTypes.Dragon4060);
+Computer ph3050 = creator.CreateComputer(ComputerTypes.Phoenix3050);
+Console.WriteLine(drg4060.VideoCard.VideoCardName);
 
 class Computer
 {
@@ -25,6 +26,10 @@ class Computer
     public ICase Case { get; set; }
 }
 
+enum ComputerTypes
+{
+    Dragon4060, Phoenix3050
+}
 class ComputerCreator
 {
     ICPU _cpu;
@@ -34,8 +39,13 @@ class ComputerCreator
     IStorage _storage;
     IPowerSupply _powerSupply;
     ICase _case;
-    public Computer CreateComputer(IComputerFactory computerFactory)
+    public Computer CreateComputer(ComputerTypes computerType)
     {
+        IComputerFactory computerFactory = computerType switch
+        {
+            ComputerTypes.Dragon4060 => new DRAGON4060(),
+            ComputerTypes.Phoenix3050 => new PHOENIX3050(),
+        }; 
         _cpu = computerFactory.CreateCPU();
         _videoCard = computerFactory.CreateVideoCard();
         _ram = computerFactory.CreateRAM();
@@ -46,6 +56,7 @@ class ComputerCreator
         return new Computer(_cpu, _videoCard, _ram, _storage, _motherBoard, _powerSupply, _case);
     }
 }
+
 class DRAGON4060 : IComputerFactory
 {
     public DRAGON4060()
@@ -349,3 +360,110 @@ enum CaseBrands
 }
 #endregion
 
+#region Hoca
+//ComputerCreator creator = new();
+//Computer asus = creator.CreateComputer(new AsusFactory());
+//Computer toshiba = creator.CreateComputer(new ToshibaFactory());
+
+//class Computer
+//{
+//    public Computer(ICPU cPU, IRAM rAM, IVideoCard videoCard)
+//    {
+//        CPU = cPU;
+//        RAM = rAM;
+//        VideoCard = videoCard;
+//    }
+//    public Computer()
+//    {
+
+//    }
+
+//    public ICPU CPU { get; set; }
+//    public IRAM RAM { get; set; }
+//    public IVideoCard VideoCard { get; set; }
+//}
+//#region Abstract Products
+//interface ICPU { }
+//interface IRAM { }
+//interface IVideoCard { }
+//#endregion
+
+//#region Concrete Products
+//class CPU : ICPU
+//{
+//    public CPU(string text)
+//        => Console.WriteLine(text);
+//}
+//class RAM : IRAM
+//{
+//    public RAM(string text)
+//        => Console.WriteLine(text);
+//}
+//class VideoCard : IVideoCard
+//{
+//    public VideoCard(string text)
+//        => Console.WriteLine(text);
+//}
+//#endregion
+
+//#region Abstract Factory
+//interface IComputerFactory
+//{
+//    ICPU CreateCPU();
+//    IRAM CreateRAM();
+//    IVideoCard CreateVideoCard();
+//}
+//#endregion
+//#region Cocnrete Factories
+//class AsusFactory : IComputerFactory
+//{
+//    public ICPU CreateCPU()
+//        => new CPU($"Asus CPU üretildi.");
+
+//    public IRAM CreateRAM()
+//        => new RAM("Asus RAM üretildi.");
+
+//    public IVideoCard CreateVideoCard()
+//        => new VideoCard("Asus Video Card Üretildi");
+//}
+//class ToshibaFactory : IComputerFactory
+//{
+//    public ICPU CreateCPU()
+//        => new CPU($"Toshiba CPU üretildi.");
+
+//    public IRAM CreateRAM()
+//        => new RAM("Toshiba RAM üretildi.");
+
+//    public IVideoCard CreateVideoCard()
+//        => new VideoCard("Toshiba Video Card Üretildi");
+//}
+//class MSIFactory : IComputerFactory
+//{
+//    public ICPU CreateCPU()
+//        => new CPU($"MSI CPU üretildi.");
+
+//    public IRAM CreateRAM()
+//        => new RAM("MSI RAM üretildi.");
+
+//    public IVideoCard CreateVideoCard()
+//        => new VideoCard("MSI Video Card Üretildi");
+//}
+//#endregion
+//#region Creator
+//class ComputerCreator
+//{
+//    ICPU _cpu;
+//    IRAM _ram;
+//    IVideoCard _videoCard;
+
+//    public Computer CreateComputer(IComputerFactory computerFactory)
+//    {
+//        _cpu = computerFactory.CreateCPU();
+//        _ram = computerFactory.CreateRAM();
+//        _videoCard = computerFactory.CreateVideoCard();
+
+//        return new(_cpu, _ram, _videoCard);
+//    }
+//}
+#endregion
+//#endregion
